9fa77d13f61580899b875e132c2bfd2a80e151b6
paul hilders <paulhilders@gmail.com>
2022-07-04 12:34:49 +0200

    Add link to github

--- app.py
+++ app.py
@@ -146,13 +146,13 @@ with demo_tabs:
     gr.Markdown("""
                 ### Acknowledgements
                 This demo was developed for the Interpretability & Explainability in AI course at the University of
-                Amsterdam. We would like express our thanks to Jelle Zuidema, Jaap Jumelet, Tom Kersten, Christos
+                Amsterdam. We would like to express our thanks to Jelle Zuidema, Jaap Jumelet, Tom Kersten, Christos
                 Athanasiadis, Peter Heemskerk, Zhi Zhang, and all the other TAs who helped us during this course.
 
                 ---
                 ### References
-                \[1\]: Chefer, H., Gur, S., & Wolf, L. (2021). Generic attention-model explainability for interpreting bi-modal and encoder-decoder transformers. In Proceedings of the IEEE/CVF International Conference on Computer Vision (pp. 397-406). <br>
+                \[1\]: Chefer, H., Gur, S., & Wolf, L. (2021). Generic attention-model explainability for interpreting bi-modal and encoder-decoder transformers. <br>
                 \[2\]: Abnar, S., & Zuidema, W. (2020). Quantifying attention flow in transformers. arXiv preprint arXiv:2005.00928. <br>
-                \[3\]: https://samiraabnar.github.io/articles/2020-04/attention_flow <br>
+                \[3\]: [https://samiraabnar.github.io/articles/2020-04/attention_flow](https://samiraabnar.github.io/articles/2020-04/attention_flow) <br>
                 """)
 demo_tabs.launch(show_error=True)
