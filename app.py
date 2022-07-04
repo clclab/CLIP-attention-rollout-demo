fbfde1a13a4a4bd26f346c1ea9060cdf85ebcdf2
paul hilders <paulhilders@gmail.com>
2022-07-04 12:11:23 +0200

    Add warning

--- app.py
+++ app.py
@@ -66,7 +66,9 @@ description = """A demonstration based on the Generic Attention-model Explainabi
                  This demo shows attributions scores on both the image and the text input when presenting CLIP with a
                  <text,image> pair. Attributions are computed as Gradient-weighted Attention Rollout (Chefer et al.,
                  2021), and can be thought of as an estimate of the effective attention CLIP pays to its input when
-                 computing a multimodal representation."""
+                 computing a multimodal representation. <span style="color:red">Warning:</span> Note that attribution
+                 methods such as the one from this demo can only give an estimate of the real underlying behavior
+                 of the model."""
 
 iface = gr.Interface(fn=run_demo,
                      inputs=inputs,
